55a813564efd212ddf97c855a5eeedfc8c0e4715
Kirana Nikita <kirananikita51@gmail.com>
2026-04-28 09:35:20 +0700

    Create Tugas_Match_Case.py

---
+++ Tugas_Match_Case.py
@@ -0,0 +1,18 @@
+barang="A01"
+
+match barang:
+  case "A01":
+    print("Nama Barang:Snack")
+    print("Harga Barang:10000")
+  case "A02":
+    print("Nama Barang:Minuman")
+    print("Harga Barang:8000")
+  case "B01":
+    print("Nama Barang:Mie Instan")
+    print("Harga Barang:3000")
+  case "B02":
+    print("Nama Barang:Sabun")
+    print("Harga Barang:5000")
+  case "C01":
+    print("Nama Barang:Shampoo")
+    print("Harga Barang:12000")
